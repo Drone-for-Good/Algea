44875d8b8008399be39df93cfaa69c5d45913f16
Heidi Kumar <heidi.kristine.hansen@gmail.com>
2015-09-24 19:52:08 -0700

    Transfer socket functions into connect. Build promises to handle server receive/response. Build helper function to get all friends from database. Build helper function to reset total stats from playerDeath.

--- server-helpers.js
+++ server-helpers.js
@@ -5,19 +5,51 @@ var db = require('models.js');
 //example function to send all user data from database
 
 exports.sendUsers = function(){
-	db.User.findAll().then(function(users){
-		io.emit('allUsers', users);
-	});
+
 };
 
 exports.sendFriends = function(username){
-
+  var friendsArray;
   //first find id for user
+  db.User.findOne({where: {'username' : username}}).then(function(user){
+    userID = user.id;
 
+    db.Friends.findAll({where: {key: user.id}}).then(function(friendsArr){
+      for (var i=0; i<friendsArr.length; i++){
+        friendsArray.push(friendsArr[i].user2.key);
+      };
+    });
+  });
   //use id to search for user in Friends database
     //give us friends User ids
 
-  //got back to User and search Friends' ids to get usernames
-
+  //go back to User and search Friends' ids to get usernames
 }
+
+exports.sendDeath = function(finalStats){
+
+  //final stats object needs to be written to database
+
+  //CHECK ALL NAMES HERE. WE ARE STILL SYNCING UP WITH ONE ANOTHER
+  //this should add to the database without needing to continue with the promise.
+  db.TotalStats.findOne({where: {key:user.id}}).then(function(currentStats){
+    currentStats.lifetime += finalStats.lifetime;
+    currentStats.score += finalStats.score;
+    currentStats.mass += finalStats.mass;
+    currentStats.totalKills += finalStats.totalKills;
+    currentStats.totalFood += finalStats.totalFood;
+    currentStats.timeInFirst += finalStats.timeInFirst;
+
+    //still needs to handle "bestScore table"
+
+  });
+
+  //for now I haven't set this to send something back to the client.
+};
+
+exports.newPlayer = function(clientInfo){
+
+};
+
+
 
