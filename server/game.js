693c7ff112f9164c05b24e6a7172d1efcf292ff3
zfreedman <zfreedman92@gmail.com>
2015-09-30 19:11:23 -0700

    Updated to Server_zf - Implemented game rooms (join/leave functionality) - Refactored setInterval update in ./server/server.js based on new game rooms

--- server/game.js
+++ server/game.js
@@ -1,28 +1,106 @@
-//global object to store current players' instantaneous data
-// need to think about how to handle rooms
-exports.allPlayerInfo = {
-  // key is socket id, for example:
-  // 12355: { lifetime: 3231, mass: 342, score: 342 ... etc },
-  // 123d5: { lifetime: 3423, mass: 343, score: 565 ... etc }
-};
-
-// Maps socket id to user id
 exports.sockets = {
-  // key is the socket id, for example:
-  // 123455: { username: Angel, userID: 123 },
-  // 123454: { username: John, userID: 125 }
+  // 123455: {
+  //   username: Angel,
+  //   userID: 123,
+  //   gameRoom: 'The Room',
+  //   skins: [],
+  //   friends: []
+  // }
 };
 
-// Look up user id from exports.socket object
-exports.getUserID = function(socketID) {
-  return exports.sockets[socketID]["userID"];
+exports.roomNames = [
+  "The Room",
+  "Hack Reactor",
+  "Freehold",
+  "San Francisco"
+];
+
+exports.roomData = {
+  defaultMaxPlayersPerRoom: 10,
+  maxRooms: 10,
+  roomCount: 0,
+  rooms: {}
 };
 
-// Look up user name from exports.socket object
-exports.getUserName = function(socketID) {
-  return exports.sockets[socketID]["username"];
+exports.addRoom = function (size) {
+  if (exports.roomData.roomCount < exports.roomData.maxRooms) {
+    var newRoom = {
+      roomName: exports.roomNames[exports.roomData.roomCount],
+      maxPlayers: size || exports.roomData.defaultMaxPlayersPerRoom,
+      playerInfo: {}
+    };
+    exports.roomData.rooms[newRoom.roomName] = newRoom;
+    exports.roomData.roomCount++;
+    console.log('\nA NEW ROOM has been CREATED.\n');
+    return newRoom;
+  } else {
+    console.log('\nThe TOTAL ROOMS CAPACITY has been REACHED.\n');
+  }
 };
 
-// Upon sign in, user is assigned to his own room with an ID == to his socketID
-// When user joins a game, we will assign his second room to the game's roomID
-// While in game, get user's roomID with socket.rooms[1] - need to verify this works
+exports.removeRoom = function (roomName) {
+  var roomRemoved = false;
+  var roomIndex = exports.roomNames.indexOf(roomName);
+  if (roomIndex !== -1 && roomName in exports.roomData.rooms) {
+    exports.roomNames
+      = exports.roomNames.slice(0, roomIndex)
+        .concat(exports.roomNames.slice(roomIndex + 1));
+    exports.roomNames.push(roomName);
+
+    delete exports.roomData.rooms[roomName];
+    roomRemoved = true;
+    console.log('\nROOM', roomName, 'REMOVED.\n');
+  } else {
+    console.log('\nROOM', roomName, 'STAYS. !@#%!@!@^!#!\n');
+  }
+  return roomRemoved;
+};
+
+exports.addRoom();
+
+exports.getValidPlayerPosition = function (roomName) {
+  var result = {x: 0, y: 0};
+  return result;
+};
+
+exports.addPlayerToRoom = function (roomName, player) {
+  if (roomName in exports.roomData.rooms
+    && (Object.keys(exports.roomData.rooms[roomName].playerInfo).length
+      < exports.roomData.rooms[roomName].maxPlayers)) {
+
+    var validPlayerPosition = exports.getValidPlayerPosition(roomName);
+    exports.roomData.rooms[roomName].playerInfo[player.username] = {
+      positionAndMass: [
+        {
+          x: validPlayerPosition.x,
+          y: validPlayerPosition.y,
+          mass: 1
+        }
+      ],
+      skin: player.skin
+    };
+
+    exports.sockets[player.socketID].gameRoom = roomName;
+
+    console.log('\nPLAYER', player.username,
+      'ADDED TO ROOM', roomName, '\n.');
+    return true;
+  } else {
+    console.log('\nPLAYER', player.username, 'COULD NOT JOIN',
+      roomName, '\n. ?!?!??!?!!!?!');
+  }
+};
+
+exports.removePlayerFromGame = function (data) {
+  var gameRoom = exports.sockets[data.socketID].gameRoom;
+  var username = exports.sockets[data.socketID].username;
+  delete exports.roomData.rooms[gameRoom].playerInfo[username];
+  exports.sockets[data.socketID].gameRoom = '';
+  return gameRoom;
+};
+
+
+exports.updatePlayer = function (data) {
+  exports.roomData.rooms[roomName].playerInfo[data.username].positionAndMass
+    = data.positionAndMass;
+};
