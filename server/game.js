00778f4ead2a08ff55ffcd4bc8e7358482605283
zfreedman <zfreedman92@gmail.com>
2015-10-01 14:23:18 -0700

    Updated on Server_zf - In ./server/game.js   - Finished documentation for currently implemented functions   - Added example to explain implementation of removeRoom

--- server/game.js
+++ server/game.js
@@ -28,6 +28,7 @@ exports.roomData = {
 
 //Add a room to roomData.rooms
 exports.addRoom = function (size) {
+  //If room limit hasn't been reached, create room
   if (exports.roomData.roomCount < exports.roomData.maxRooms) {
     var newRoom = {
       roomName: exports.roomNames[exports.roomData.roomCount],
@@ -43,15 +44,32 @@ exports.addRoom = function (size) {
   }
 };
 
+//Remove a room
+//NOTE ON CLEVER THING:
+/*
+Roomnames are picked by using exports.roomData.roomCount
+as the index to reference a name from exports.roomNames
+
+Suppose roomnames are ['a', 'b', 'c', 'd'], and 'a', 'b', 'c'
+are currently up and running. If we turn off room 'b' (0 players),
+now exports.roomData.roomCount will be decremented to 2. When
+We need to create a new room, we will try and create
+['a', 'b', 'c', 'd'][roomData.roomCount === 2] === 'c', but
+'c' is already in use.
+
+To avoid this, when we remove a room, we get the roomname ('b'),
+remove it from exports.roomNames using Array.splice(), and
+push it onto the end of exports.roomNames. This way, in the
+example above, after removing a room, exports.roomNames would be
+['a', 'c', 'd', 'b'], and exports.roomNames[2] is 'd', which is
+not in use
+*/
 exports.removeRoom = function (roomName) {
   var roomRemoved = false;
   var roomIndex = exports.roomNames.indexOf(roomName);
   if (roomIndex !== -1 && roomName in exports.roomData.rooms) {
-    exports.roomNames
-      = exports.roomNames.slice(0, roomIndex)
-        .concat(exports.roomNames.slice(roomIndex + 1));
+    exports.roomNames.splice(roomIndex, 1);
     exports.roomNames.push(roomName);
-
     delete exports.roomData.rooms[roomName];
     roomRemoved = true;
     console.log('\nROOM', roomName, 'REMOVED.\n');
@@ -61,18 +79,25 @@ exports.removeRoom = function (roomName) {
   return roomRemoved;
 };
 
+//Add a room when starting the server
 exports.addRoom();
 
+//Calculate a valid player position to create
+//a new player bsaed off of the current players in the room
 exports.getValidPlayerPosition = function (roomName) {
   var result = {x: 0, y: 0};
   return result;
 };
 
+//Put a player in a new room
 exports.addPlayerToRoom = function (roomName, player) {
+  //If the roomName exists and
+  //the player count of the room is under the max
   if (roomName in exports.roomData.rooms
     && (Object.keys(exports.roomData.rooms[roomName].playerInfo).length
       < exports.roomData.rooms[roomName].maxPlayers)) {
 
+    //Make a player
     var validPlayerPosition = exports.getValidPlayerPosition(roomName);
     exports.roomData.rooms[roomName].playerInfo[player.username] = {
       positionAndMass: [
@@ -96,6 +121,7 @@ exports.addPlayerToRoom = function (roomName, player) {
   }
 };
 
+//Remove a player from their current game room
 exports.removePlayerFromGame = function (data) {
   var gameRoom = exports.sockets[data.socketID].gameRoom;
   var username = exports.sockets[data.socketID].username;
@@ -104,8 +130,10 @@ exports.removePlayerFromGame = function (data) {
   return gameRoom;
 };
 
-
+//Update a player by referencing data on the client
+// { username, roomName, positionAndMass }
 exports.updatePlayer = function (data) {
-  exports.roomData.rooms[roomName].playerInfo[data.username].positionAndMass
+  exports.roomData.rooms[data.roomName]
+    .playerInfo[data.username].positionAndMass
     = data.positionAndMass;
 };
