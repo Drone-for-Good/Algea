e133069628998179e0cbf7c5c128edfda16d0e04
Tad <tad@whitakerresearch.com>
2015-10-07 19:31:28 -0700

    Adding Procfile and server environment variable.

--- server/server.js
+++ server/server.js
@@ -266,6 +266,10 @@ var findFriendsOnlineAndNotify = function (baseUser, friends, status) {
   }
 };
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+// http.listen((process.env.PORT || 3000), function(){
+//   console.log('listening on', http.address().port);
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
